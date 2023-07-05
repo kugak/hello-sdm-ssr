68c38b5eb773f35eecf8ad7559bb0737a0fe8cc4
kuga <contact@kuga.ca>
2023-07-05 15:58:05 -0400

    added 24hr cache to google api

--- src/routes/hours/+page.server.js
+++ src/routes/hours/+page.server.js
@@ -1,10 +1,18 @@
-import { PUBLIC_GM_API_KEY, PUBLIC_DD_API_KEY } from "$env/static/public";
+import { PUBLIC_DD_API_KEY, PUBLIC_GM_API_KEY } from "$env/static/public";
 
 export const load = ({ fetch, url }) => {
-  
+  let cachedHours = null;
+  let lastFetchTime = null;
+
   const fetchHours = async () => {
     const searchParams = new URLSearchParams(url.search);
     const placeId = searchParams.get("place_id") || "ChIJu8u_gHVhK4gREY8XyWcrJ8k";
+
+    // Check if the cached hours are still valid (within 24 hours)
+    if (cachedHours && lastFetchTime && Date.now() - lastFetchTime < 24 * 60 * 60 * 1000) {
+      return cachedHours;
+    }
+
     try {
       const res = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${PUBLIC_GM_API_KEY}&fields=current_opening_hours`);
 
@@ -15,17 +23,17 @@ export const load = ({ fetch, url }) => {
       }
 
       const data = await res.json();
-    //   console.log('ssr');
-    //   console.log(data.result.opening_hours);
-    //   console.log('end ssr');
+      const hours = data.result.current_opening_hours;
 
-      return data.result.current_opening_hours;
+      // Cache the fetched hours and update the last fetch time
+      cachedHours = hours;
+      lastFetchTime = Date.now();
+
+      return hours;
     } catch (error) {
       // Log the error to DataDog
       const datadogApiKey = `${PUBLIC_DD_API_KEY}`;
       const datadogEndpoint = 'https://api.datadoghq.com/api/v1/events';
-    //   const datadogTags = ['app:sdm-ssr', 'env:production'];
-
       const logPayload = {
         title: "Error fetching data from Google Maps API",
         text: error.message,
@@ -42,7 +50,7 @@ export const load = ({ fetch, url }) => {
           `version:${placeId}`,
           "event:fetchData",
           `tag:'hoursPage-'${placeId}`,
-      ],
+        ],
       };
 
       await fetch(datadogEndpoint, {
