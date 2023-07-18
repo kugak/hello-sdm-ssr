c56fca62626aa7096b3f821018ceca0903a363fe
Kuga K <contact@kuga.ca>
2023-07-18 15:53:10 -0400

    updated time container width

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -245,7 +245,7 @@
       background-repeat: no-repeat;
       background-size: cover;
       background-position: center;
-      width: 450px;
+      width: 550px;
       height: 400px;
       position: absolute;
       display: -webkit-box;
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -222,7 +222,7 @@
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
-    width: 450px;
+    width: 550px;
     height: 400px;
     position: absolute;
     display: -webkit-box;
